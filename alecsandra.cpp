a6770510b216391eb4fa879cc6e5006c9657d13e
Daniel Oraca <danieloraca@users.noreply.github.com>
2022-09-26 08:29:05 +0100

    take 2

--- alecsandra.cpp
+++ alecsandra.cpp
@@ -1,9 +1,18 @@
-// Online C++ compiler to run C++ program online
 #include <iostream>
 #include <algorithm>
 #include <iterator>
 
 using namespace std;
+
+bool exists(int k, int gri[], int val) {
+    for(int i = 0; i < k; i++){
+        if(gri[i] == val){
+            return true;
+        }
+    }
+    
+    return false;
+}
 
 int main() {
     int n, k;
@@ -29,16 +38,9 @@ int main() {
         cin >> gri[i];
     }
     
-    //dale gri
-    for (int i = 1; i <= k; i++) {
-        cout << gri[i] << "|";
-    }
-    cout << "\n";
-    
     for (int i = 1; i <= n; i++) {
         for (int j = i*(i-1)/2+1; j <= i*(i+1)/2; j++) {
-            bool exists = std::find(gri, gri + k, j) != gri + k;
-            if (!exists) {
+            if (!exists(k, gri, j)) {
                 cout << ":" << j;
             } else {
                 cout << ":X";
@@ -46,8 +48,6 @@ int main() {
         }
         cout << "\n";
     }
-    
-    cout << "GATA";
 
     return 0;
 }
